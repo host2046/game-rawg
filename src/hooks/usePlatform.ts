005328bd58cc5c6f67e0c52c31ed122d0fb37871
ali mizbani <alimizbani888@gmail.com>
2023-05-28 18:57:33 +0430

    create look up hooks

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,8 +1,8 @@
-import paltforms from "../data/paltform";
 import { useQuery } from "@tanstack/react-query";
+import paltforms from "../data/paltform";
 
-import { Platform } from "./useGame";
 import ApiClient from "../services/api-client";
+import { Platform } from "./useGame";
 
 const apiClient = new ApiClient<Platform>("/platforms/lists/parents");
 const usePlatform = () =>
@@ -10,7 +10,7 @@ const usePlatform = () =>
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
-    initialData: { count: paltforms.length, results: paltforms },
+    initialData: paltforms,
   });
 
 export default usePlatform;
